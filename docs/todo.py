589582f6589c43ddc9267eb5348c62945ed4c564
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2026-03-22 16:40:52 +0100

    bug fixes

---
+++ docs/todo.py
@@ -0,0 +1,15 @@
+############## Page improvement
+# TODO STL of each part and complete in a zip file
+# TODO address of NIH model
+# TODO integrate models.html as page
+# TODO create a github discussion place
+# TODO page of general troubleshoot
+
+
+############## Material
+# TODO video of assembling
+# TODO photo of the 3d printed camera holder (eye-facing) with and without the camera
+# TODO photo of the 3d printed camera holder (world-facing) with and without the camera
+# TODO video or soldering the LEDs
+# TODO photo of IR cut filter removal
+# TODO photo of the resistance and cable management
