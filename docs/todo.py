5f71ab5ae5316f0e401e643a4606b40393f52d30
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2026-03-24 22:45:10 +0100

    get started updated

--- docs/todo.py
+++ docs/todo.py
@@ -1,12 +1,13 @@
 ############## Page improvements
 # ✓ TODO STL of each part and complete in a zip file
 # ✓ TODO address of NIH model
-# TODO integrate models.html as page
-# TODO create a github discussion place
-# TODO page of general troubleshoot
+# ✓ TODO integrate models.html as page
 # TODO example code
 # TODO insert university affiliations
 # TODO a better icon??
+# TODO chin rest?
+# TODO create a github discussion place
+# TODO page of general troubleshoot
 
 
 ############## Material
@@ -14,6 +15,6 @@
 # TODO video of assembling
 # TODO photo of the 3d printed camera holder (eye-facing) with and without the camera
 # TODO photo of the 3d printed camera holder (world-facing) with and without the camera
-# TODO video or soldering the LEDs
+# TODO video or photos of soldering the LEDs
 # TODO photo of IR cut filter removal
 # TODO photo of the resistance and cable management
