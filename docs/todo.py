ba22b484cf77bd0ebe595bcd8a20b415d34721ab
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2026-03-28 18:55:48 +0100

    icons
    and 404 page

--- docs/todo.py
+++ docs/todo.py
@@ -1,18 +1,13 @@
 ############## Page improvements
-# ✓ TODO load the STL of the  external light source
 # TODO example code
 # TODO insert university affiliations
 # TODO a better icon??
-# TODO chin rest?
 # TODO create a github discussion place
 # TODO page of general troubleshoot
-# TODO bambulab in the mainpage
 # TODO add chin rest reference in resources: https://3dgo.app/models/thingiverse/2968729
-# TODO in the list of 3d pritntable parts insert left and right ear support and other parts that are bilaterally different
 # TODO update 3mf with the correct layout of print (arms horizontal)
 
 ############## Material
 # TODO video of the eye with and without pupillometry
 # TODO video or photos of soldering the LEDs
-# TODO photo of IR cut filter removal
 # TODO photo of the resistance and cable management
