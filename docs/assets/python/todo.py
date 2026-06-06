ea8c7012a674b1858a7c7d3d0bedb6103e0c17a9
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2026-06-07 00:43:21 +0200

    todo update

--- docs/assets/python/todo.py
+++ docs/assets/python/todo.py
@@ -1,6 +1,6 @@
 ############## Page improvements
 
-# TODO create a new documentation for meye[PT]
+
 # TODO add examples
 # TODO example code: add an example in which the model is different from default
 # TODO page of general troubleshoot
