7acf51e70c4b120a9dfc28833132a17fc24f45ac
raffaelemazziotti <46002983+raffaelemazziotti@users.noreply.github.com>
2026-06-17 22:57:58 +0200

    todo update

--- docs/assets/python/todo.py
+++ docs/assets/python/todo.py
@@ -1,10 +1,6 @@
 ############## Page improvements
 
-# TODO il selector su cellulare non va bene
-# TODO model zoo su cellulare tipo cards
-# TODO tutorial per cellulare tipo cards
-# TODO how to change the focus of the camera
-# TODO page of general troubleshoot?
+# TODO page or section on general troubleshoot?
 
 
 ############## Material
